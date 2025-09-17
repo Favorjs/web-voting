7120b89882817b02a1ec6b2b8919a7f4280e7371
Favorjs <107522010+Favorjs@users.noreply.github.com>
2025-09-17 16:21:05 +0100

    Update ResultsPage.jsx

--- src/components/ResultsPage.jsx
+++ src/components/ResultsPage.jsx
@@ -8,8 +8,8 @@ import { Chart as ChartJS, ArcElement, BarElement, CategoryScale, LinearScale, T
 ChartJS.register(ArcElement, BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 import { API_URL } from '../config';
 
-const DEFAULT_PROXY_VOTES = 477;
-const DEFAULT_PROXY_HOLDINGS = 14628202296;
+const DEFAULT_PROXY_VOTES = 538;
+const DEFAULT_PROXY_HOLDINGS = 549180010;
 
 
 
