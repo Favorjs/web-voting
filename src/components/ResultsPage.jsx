7920ff9366c2e54e6ec5d0b2dbcdb6d7f22696d6
Favorjs <107522010+Favorjs@users.noreply.github.com>
2025-09-17 13:32:01 +0100

    Update ResultsPage.jsx

--- src/components/ResultsPage.jsx
+++ src/components/ResultsPage.jsx
@@ -412,7 +412,7 @@ const fetchActiveAuditMember = async () => {
     <div className="results-container">
       <div className="header">
         <img src="/favicon.png" alt="Left Logo" />
-        <img src="/redstar.jpg" alt="Right Logo" />
+        <img src="/redstar.png" alt="Right Logo" />
       </div>
       
       <h2>Voting Results</h2>
